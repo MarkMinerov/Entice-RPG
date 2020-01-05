fca164c0afc1381bf9d4eddfa9995469be46e15a
Mark Minerov <markminerov123@gmail.com>
2020-01-05 15:08:16 +0200

    Сайт для самп сервера

---
+++ js/common.js
@@ -0,0 +1,23 @@
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
+
+function CopyToClipboard(containerid) {
+    if (document.selection) { 
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select().createTextRange();
+        document.execCommand("copy"); 
+    
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        alert("text copied") 
+}}
---
+++ js/vue.js
@@ -0,0 +1,47 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        players: null,
+        maxPlayers: null,
+        news: [
+            {
+                header: 'Заголовок 1',
+                description: 'Какой-то текст',
+                time: ''
+            },
+
+            {
+                header: 'Заголовок 2',
+                description: 'Какой-то текст',
+                time: ''
+            },
+
+            {
+                header: 'Заголовок 3',
+                description: 'Lorem Ipsum является текст-заполнитель обычно используется в графических, печать и издательской индустрии для предварительного просмотра макета и визуальных макетах.',
+                time: ''
+            },
+
+        ]
+    },
+
+    filters: {
+       sliceString: (value, length) => `${value}`.length > length ? `${`${value}`.substr(0, length).trim()}...` : `${value}`,
+    },
+  
+    mounted() {
+        axios.get('http://monitor.sacnr.com/api/?IP=176.32.39.8&Port=7777&Action=info&Format=JSON')
+            
+            .then(res => {
+                this.players = (res.Players || 1);
+                this.maxPlayers = (res.MaxPlayers || 1);
+            })
+
+            .catch( () => {
+                this.players = 1
+                this.maxPlayers = 1;
+            })
+    }
+
+})
